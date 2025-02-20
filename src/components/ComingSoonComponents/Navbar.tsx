e6789f88388905e2f3130c823aeb0f606d8a0d0a
DevDhawan <devdhawan2004@gmail.com>
2025-02-19 18:34:37 -0800

    Removal of useNavigate

--- src/components/ComingSoonComponents/Navbar.tsx
+++ src/components/ComingSoonComponents/Navbar.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavbarStyles = styled.nav`
   background: rgba(0, 0, 0, 0);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavbarStyles = styled.nav`
   background: white;
