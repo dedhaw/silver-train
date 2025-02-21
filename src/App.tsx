57c97f4efaa1e8440666e4244e95c87ac6d05467
DevDhawan <devdhawan2004@gmail.com>
2025-02-21 13:57:27 -0800

    Coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "./views/Landing";
-import PNF from "./views/PageNotFound";
+// import Landing from "./views/Landing";
+// import PNF from "./views/PageNotFound";
 import ComingSoon from "./views/ComingSoon";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
+      {/* <Route path="/" element={<Landing />} />
       <Route path="/*" element={<PNF />} />
-      <Route path="/about" element={<ComingSoon />} />
+      <Route path="/about" element={<ComingSoon />} /> */}
+      <Route path="/" element={<ComingSoon />} />
     </Routes>
   );
 }
